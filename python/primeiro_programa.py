0c134f950f681811a264e534b323c41ca0ae552d
omdouglas96 <douglas.mesquita@icloud.com>
2026-08-28 21:31:06 -0300

    Add age check and greeting to primeiro_programa.py
    Adiciona condição de maioridade

--- python/primeiro_programa.py
+++ python/primeiro_programa.py
@@ -3,4 +3,10 @@ idade = int(input("Qual é a sua idade?"))
 
 print(f"Ola, {nome}!")
 print(f"Voce tem {idade} anos.")
+
+if idade >= 18:
+  print("Voce e maior de idade.")
+else:
+  print("Voce e menor de idade.")
+  
 print ("Estou estudando Python!")
